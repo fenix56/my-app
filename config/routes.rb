ccff57e47e6149b60cd117ea6fc080a9661b7e6b
Przemek <chall3ng3r91@gmail.com>
2021-12-11 18:24:02 +0000

    Getting some data from API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'albums#index'
+  get 'albums/:id', to: 'albums#show' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
