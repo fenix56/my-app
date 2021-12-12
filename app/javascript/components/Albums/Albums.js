3b95dd0e18be6a18972364b88287a2d229b8c873
Przemek <chall3ng3r91@gmail.com>
2021-12-12 12:33:09 +0000

    Getting data from Rails to React

--- app/javascript/components/Albums/Albums.js
+++ app/javascript/components/Albums/Albums.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Albums = () => {
-    return (<div>This is an Albums view for app.</div>)
+    const [albums, setAlbums] = useState([])
+
+    useEffect(() => {
+        axios.get('api/v1/albums.json')
+        .then( response => console.log(response) )
+        .catch( response => console.log(response) )
+    }, [albums.length])
+
+    return (
+     <div>This is the Albums#index view for our app.</div>
+    )
 }
 
 export default Albums;
