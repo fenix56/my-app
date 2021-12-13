daece2c047e48043090418582134ecd062929309
Przemek <chall3ng3r91@gmail.com>
2021-12-13 15:38:10 +0000

    will_paginate Albums first approach

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -1,13 +1,25 @@
 module Api
   module V1
     class AlbumsController < ApplicationController
-    
+      
+
+
       def index
         @albums = Album.all
         @users = User.all
         @albums.each do |album|
           album.user = @users.find { |user| user.id == album.userId }
         end
+
+        unless (params[:page].blank?) 
+          @albums = @albums.paginate(page: params[:page], per_page: 10)
+          return render json: {
+            data: @albums,
+            page: @albums.current_page,
+            pages: @albums.total_pages 
+          }
+        end
+
         render json: @albums
       end
     
@@ -15,6 +27,7 @@ module Api
         @album = Album.find(params[:id])
         @album.user = User.find(@album.userId)
         @album.photos = Photo.all(@album.id)
+        
         render json: @album.to_json
       end
     end
--- app/javascript/components/Albums/Album.js
+++ app/javascript/components/Albums/Album.js
@@ -17,7 +17,7 @@ const Album = (props) => {
                 <img className="card-img-top" src="https://via.placeholder.com/150/877cd8" />
                 <div className="card-body">
                     <h5 className="card-title" style={titleStyle}>{props.attributes.title}</h5>
-                    <p className="card-text">This album is owned by <b>{props.attributes.name}</b></p>
+                    <p className="card-text">This album is owned by <b>{props.attributes.user.name}</b></p>
                     <Link to={`/albums/${props.attributes.id}`} className="btn btn-primary">View Album</Link>
                 </div>
             </div>
