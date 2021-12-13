36fe17301aa00c7f4ddcd8a36dee55044eac3a35
Przemek <chall3ng3r91@gmail.com>
2021-12-13 18:42:01 +0000

    Pagination WIP

--- app/javascript/components/Albums/Albums.js
+++ app/javascript/components/Albums/Albums.js
@@ -1,40 +1,48 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import Album from "./Album";
 
 const Albums = () => {
-    const [albums, setAlbums] = useState([])
+  const [albums, setAlbums] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
 
-    useEffect(() => {
-        axios.get('api/v1/albums')
-        .then( resp => {
-            setAlbums(resp.data)
-        })
-        .catch( resp => console.log(resp) )
-    }, [albums.length])
+  const { page } = useParams();
 
-    const grid = albums.map( item => {
-        return (
-            <Album 
-                key={item.id}
-                attributes={item}
-            />
-        )
-    })
+  let page_str = "page=1"
 
+  if (page) {
+    page_str = page
+  }
+
+  useEffect(() => {
+    axios.get(`api/v1/albums?${page_str}`)
+      .then(resp => {
+        setAlbums(resp.data.data)
+        setCurrentPage(resp.data.page)
+      })
+      .catch(resp => console.log(resp))
+  }, [])
+
+  console.log(albums)
+
+  const grid = albums.map(item => {
     return (
-        <div className="home">
-            <div className="header">
-                <h1>Albums List</h1>
-                <div className="sub-header">This is the Albums#index view for our app.</div>
-            </div>
-            <br/>
-            <div className="container">
-                <div className="row">{grid}</div>    
-            </div>
-            
-        </div>
+      <Album
+        key={item.id}
+        attributes={item}
+      />
     )
+  })
+
+  return (
+    <div>
+      <br />
+      <div className="container">
+        <div className="row">{grid}</div>
+      </div>
+    </div>
+  )
 }
 
 export default Albums;
--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,19 +1,41 @@
 import React from 'react'
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Albums from './Albums/Albums'
 import Album from './Album/Album'
 
 const App = () => {
-    return(
-      <Router>
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <div className="navbar navbar-dark bg-dark shadow-sm">
+            <div className="container">
+              <Link to="/" className="navbar-brand d-flex align-items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" aria-hidden="true" className="me-2" viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" /><circle cx="12" cy="13" r="4" /></svg>
+                <strong>Album</strong>
+              </Link>
+            </div>
+          </div>
+        </header>
+
         <Routes>
-          <Route path="/" element={<Albums />}></Route> 
-          <Route path="/albums" element={<Albums />}></Route> 
+          <Route exact path="/" element={<Albums />}></Route>
+          <Route exact path="/albums" element={<Albums />}></Route>
           <Route exact path="/albums/:id" element={<Album />}></Route>
         </Routes>
-      </Router>
-    )
+
+        <footer className="text-muted py-5">
+          <div className="container">
+            <p className="float-end mb-1">
+              <a href="#">Back to top</a>
+            </p>
+            <p className="mb-1">Album example is © Bootstrap, but please download and customize it for yourself!</p>
+          </div>
+        </footer>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
